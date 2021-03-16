4ad4c773c64f70897c31b67f341b926449fbba9d
Raju979 <thaparaju979@gmail.com>
2021-03-16 11:30:29 +0545

    changed layout for homework

--- app/src/main/java/com/example/raju/MainActivity.java
+++ app/src/main/java/com/example/raju/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.raju;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
     private TextView mShowCount;
+    private Button countButton,zeroButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +32,19 @@ public class MainActivity extends AppCompatActivity {
         if(mShowCount !=null){
             mShowCount.setText(Integer.toString(mCount));
         }
+//        if(mCount%2 !=0){
+            countButton = (Button) findViewById(R.id.button_count);
+//            View root = countButton.getRootView();
+        countButton.setBackgroundColor(getResources().getColor(R.color.black));
+        zeroButton = findViewById(R.id.button_zero);
+        zeroButton.setBackgroundColor(Color.WHITE);
+        ((Button)view).setBackgroundColor(Color.WHITE);
+
+//        }
+    }
+
+    public void countZero(View view) {
+        mCount = 0;
+        mShowCount.setText(Integer.toString(mCount));
     }
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,2 @@
-Task 3: Change the layout to RelativeLayout
-!['Converted to relative layout'](./task3_relative_layout.JPG)
-!['Relative layout in emulator'](./task3_relative_layout_emulator.JPG)
+## Homework
+!['Converted to relative layout'](./homework_1.JPG)
